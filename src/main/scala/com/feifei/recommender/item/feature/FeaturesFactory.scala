0fdf2af7f7fb189f3f0274320e0703ff64c40ef3
程飞 <chengfei@newland.com.cn>
2021-03-11 14:37:46 +0800

    特征工程的构建

--- src/main/scala/com/feifei/recommender/item/feature/FeaturesFactory.scala
+++ src/main/scala/com/feifei/recommender/item/feature/FeaturesFactory.scala
@@ -8,20 +8,21 @@ import org.apache.spark.ml.feature.VectorAssembler
 import org.apache.spark.ml.linalg.{DenseVector, SparseVector}
 import org.apache.spark.rdd.RDD
 import com.feifei.recommender.item.util.{PropertiesUtils, SparkSessionBase}
+import org.apache.hadoop.conf.Configuration
 
 import scala.collection.mutable.ListBuffer
-
+/**
+  * 构建训练集-特征工程
+  */
 object FeaturesFactory {
 
   //构建特征工程
   def getLRFeatures = {
-    /**
-      * 构建训练集-特征工程
-      */
+
     val session = SparkSessionBase.createSparkSession()
     session.sparkContext.setLogLevel("error")
     val table = PropertiesUtils.getProp("user.profile.hbase.table")
-    val conf = HBaseConfiguration.create()
+    val conf: Configuration = HBaseConfiguration.create()
     conf.set("hbase.zookeeper.property.clientPort", PropertiesUtils.getProp("hbase.zookeeper.property.clientPort"))
     conf.set("hbase.zookeeper.quorum", PropertiesUtils.getProp("hbase.zookeeper.quorum"))
     conf.set("zookeeper.znode.parent", PropertiesUtils.getProp("zookeeper.znode.parent"))
@@ -34,8 +35,8 @@ object FeaturesFactory {
     hbaseRdd = hbaseRdd.cache()
     import session.implicits._
 
-
-    //读取hbase信息,扫描信息
+    //读取hbase信息,扫描信息 ((userID), (itemID, time, keywords, score, province, city))
+    //得到节目关键词的词袋
     var distinctWords = hbaseRdd.flatMap(data => {
       val list = new ListBuffer[String]
       val result = data._2
@@ -64,6 +65,7 @@ object FeaturesFactory {
 
     val distinctWordsBroad = session.sparkContext.broadcast(distinctWords)
 
+    //利用词袋模型，把用户画像数据进行向量化
     val labelFeatures = hbaseRdd.flatMap(data => {
       val result = data._2
       val dict = distinctWordsBroad.value
@@ -91,13 +93,15 @@ object FeaturesFactory {
 
           //创建一个词袋大小的向量，并在词袋中有的关键字，节目中也有的对应的索引位置上填上score值，其他的为0
           val vector = new SparseVector(dict.size, indexs, Array.fill(indexs.length)(score))
+
+          //得到用户对某个节目的向量化
           list.+=(((userID, itemID), vector.toDense))
         }
       }
       list.iterator
     })
 
-
+    //得到用户基本属性，城市和省份的词袋
     val provinceWithCity = hbaseRdd.map(data => {
       val result = data._2
       var userID = ""
@@ -122,6 +126,7 @@ object FeaturesFactory {
     val provinceMapBroad = session.sparkContext.broadcast(provinceMap)
     val cityMapBroad = session.sparkContext.broadcast(cityMap)
 
+    //利用词袋模型，对城市和省份进行向量化
     val provinceWithCityFeatures = provinceWithCity.map(data => {
       val userID = data._1
       val province = data._2._1
@@ -133,6 +138,8 @@ object FeaturesFactory {
       println(city)
       val cityIndex = Array(cityMap.get(city).get.toInt)
       val cityFeatures = new SparseVector(cityMap.size, cityIndex, Array.fill(cityIndex.length)(1.0))
+
+      //得到用户基本信息中的省份城市的向量化
       (userID, (provinceFeatures.toDense, cityFeatures.toDense))
     })
 
@@ -146,7 +153,7 @@ object FeaturesFactory {
       "from recommender.user_action a join " +
       "tmp_program.tmp_keyword_weight b " +
       "on (a.item_id = b.item_id) ")
-    itemFeatureDF.show()
+//    itemFeatureDF.show()
 
     /**
       * root
@@ -155,6 +162,8 @@ object FeaturesFactory {
       * |-- duration: long (nullable = true)
       * |-- features: vector (nullable = true)
       */
+
+    //得到节目画像向量化后的数据
     val userID2ActionRDD = itemFeatureDF.rdd.map(row => {
       val sn = row.getAs[String]("sn")
       val itemID = row.getAs[Long]("item_id").toInt
@@ -163,7 +172,7 @@ object FeaturesFactory {
       (sn, (itemID, duration, features))
     })
 
-
+    //拿到节目的总时长，为了后面根据观看比例，判断喜不喜欢，作为lable标签使用
     val itemInfo = session.table("recommender.item_info")
     val itemID2LengthMap = itemInfo.map(row => {
       val itemID = row.getAs[Long]("id").toInt
@@ -174,7 +183,10 @@ object FeaturesFactory {
     //由于节目信息数据量并不是很大，完全可以放入在广播变量中保存
     val itemID2LengthMapBroad = session.sparkContext.broadcast(itemID2LengthMap)
 
+
+    //构建完整的训练集数据（用户画像数据，节目画像数据，标签数据）
     val featuresDF = userID2ActionRDD.join(provinceWithCityFeatures).map(row => {
+      //把节目画像向量数据和用户基本属性向量化后的数据整合在一起
       val userID = row._1
       val (itemID, duration, features) = row._2._1
       val (provinceVector, cityVector) = row._2._2
@@ -187,7 +199,7 @@ object FeaturesFactory {
 
         val itemID2LengthMap = itemID2LengthMapBroad.value
         val length = itemID2LengthMap.get(itemID).get
-        // TODO: 数据需要修改
+        // TODO: 数据需要修改，计算标签数据，根据逗留的时间，结合预先规定的判断标准判断喜不喜欢，是1表示喜欢，0表示不喜欢
         val label = if (duration < length) {
           val durationScale = (duration * 1.0) / length
           if (durationScale > 0.1) 1 else 0
@@ -202,6 +214,9 @@ object FeaturesFactory {
         .setInputCols(Array("program_features", "province_Vector", "city_Vector", "userLabel_Vector"))
         .setOutputCol("features")
         .transform(featuresDF)
+    println("++++++++++++++++++++++++++++++++++++")
+    trainDF.show(false)
+
     trainDF
   }
 
